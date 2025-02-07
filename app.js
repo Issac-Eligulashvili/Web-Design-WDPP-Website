7cc95f25e95d0a3a1d25975b0b7ef6053a44657f
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-02-06 21:11:01 -0500

    finished ti for the mobile screen

--- app.js
+++ app.js
@@ -22,7 +22,26 @@ $(document).ready(function () {
           $(".carouselActive").addClass("animate__fadeOutDown")
           $(".carouselActive").removeClass("carouselActive animate__fadeInDown"); 
           $(`.webCarouselContent[data-index='${$(event.currentTarget).data("year") - 1}']`).addClass("carouselActive animate__fadeInDown");
-          console.log(previousElementIndex);
+          setTimeout(() => {
+          $(`.webCarouselContent[data-index='${previousElementIndex}']`).removeClass("animate__fadeOutDown");
+          }, 1000)
+     })
+
+     // Navigation bar for course progression on mobile
+     $(".logoMobile").on('click', (event) => {
+          $(".logoMobile").removeClass("active"); //removing the class from all elements
+          $(event.currentTarget).addClass("active");//adding the box shadow affect to the clicked element
+          const index = $(event.currentTarget).data("num-items"); //getting the numitems data to calculate the height of the navbar properly
+          // calculating the height of the navbar based on the num items and jQuery
+          $(".overlay.mobile").animate({
+               "width": `${100 * index / 6}%`
+          }, 200)
+          // implementing the navigation system of the Year Projects Carousel
+          const previousElementIndex = $(".carouselActive").data("index");
+          
+          $(".carouselActive").addClass("animate__fadeOutDown")
+          $(".carouselActive").removeClass("carouselActive animate__fadeInDown"); 
+          $(`.webCarouselContent[data-index='${$(event.currentTarget).data("year") - 1}']`).addClass("carouselActive animate__fadeInDown");
           setTimeout(() => {
           $(`.webCarouselContent[data-index='${previousElementIndex}']`).removeClass("animate__fadeOutDown");
           }, 1000)
